05c17b45abb6674b8320e0ba5c31473591d7c7be
mattew-me <mateus2015men@outlook.com>
2021-12-02 04:59:25 -0300

    add project

---
+++ deteccaoplacas.py
@@ -0,0 +1,96 @@
+import cv2
+import numpy as np
+import glob
+import math
+
+imagefiles = glob.glob("cars_plates/*")
+for filename in imagefiles:
+    img = cv2.imread(filename)
+    img_copia = img.copy()
+
+    img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    img_cinza = cv2.GaussianBlur(img_cinza, (3,3), 0.2)
+    kernel = np.array([[-1,-1,-1],[-1,9,-1],[-1,-1,-1]])
+    img_laplacian = cv2.filter2D(img_cinza, -1, kernel) 
+
+    img_gauss = cv2.GaussianBlur(img_laplacian, (3,3), 0)
+
+    ret, thresh = cv2.threshold(img_gauss, 90, 255, type=cv2.THRESH_OTSU)
+
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(img, contours, -1, (255, 0, 0), 1)
+
+    #obtendo apenas os retangulos de certo tamanho
+    lista = []
+    for rec in contours:
+        x,y,w,h = cv2.boundingRect(rec)
+        if h>17 and h<35 and w>3 and w<50:
+            cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 1)
+            lista.append([x,y,x+w,y+h])
+
+    #eliminando os retangulos sobrepostos
+    lista_2 = []
+    for i in range(len(lista)):
+        flag = 0
+        for j in range(len(lista)):
+            if i!=j:
+                if lista[i][0]>lista[j][0] and lista[i][0]<lista[j][2] and (lista[i][1]>=lista[j][1] and lista[i][1]<=lista[j][3] or lista[i][3]>=lista[j][1] and lista[i][3]<=lista[j][3]):
+                    cv2.rectangle(img, (lista[i][0], lista[i][1]),(lista[i][2], lista[i][3]),(0,255,255),1)                    
+                    flag=1
+        if flag==0:
+            lista_2.append(lista[i])
+
+    #eliminando os retangulos nao alinhados com a maioria
+    lista_3 = []
+    for i in range(len(lista_2)):
+        cont=0
+        for j in range(len(lista_2)):
+            if i!=j and math.fabs(lista_2[i][1]-lista_2[j][1])<10 and math.fabs(lista_2[i][3]-lista_2[j][3])<10:
+                cont+=1
+        if cont>=len(lista_2)*.4:
+            lista_3.append(lista_2[i])        
+
+
+    for i in range(len(lista_3)):
+        cv2.rectangle(img, (lista_3[i][0], lista_3[i][1]),(lista_3[i][2], lista_3[i][3]),(0,0,255),3) 
+
+    #obtendo a regiao da placa
+    xi=lista_3[0][0]
+    yi=lista_3[0][1]
+    xf=lista_3[0][2]
+    yf=lista_3[0][3]
+    for i in range(len(lista_3)):
+        if lista_3[i][0]<xi:
+            xi=lista_3[i][0]
+        if lista_3[i][1]<yi:
+            yi=lista_3[i][1]    
+        if lista_3[i][2]>xf:
+            xf=lista_3[i][2]
+        if lista_3[i][3]>yf:
+            yf=lista_3[i][3]    
+    yi-=7
+    yf+=7
+    dif=int((330-(xf-xi))/2)
+    xi=max(0,xi-dif)
+    xf=min(img.shape[1]-1,xf+dif)
+    cv2.rectangle(img, (xi,yi),(xf,yf),(0,0,0),3)
+
+    img_placa=img_copia[yi:yf,xi:xf]
+
+    rows, cols, _ = img_placa.shape
+
+    img_placa_cinza = cv2.cvtColor(img_placa, cv2.COLOR_BGR2GRAY)
+
+    ret_placa, thresh_placa = cv2.threshold(img_placa_cinza, 105, 255, type=cv2.THRESH_OTSU)
+
+    # contours_placa, hierarchy_placa = cv2.findContours(thresh_placa, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    # cv2.drawContours(img_placa, contours_placa, -1, (255, 0, 0), 1)
+
+    cv2.imshow(filename+"_placa",thresh_placa)    
+
+    """ cv2.imshow(filename,img) """
+    cv2.waitKey(0)
+
+
+
---
+++ reconhecedigitos.py
@@ -0,0 +1,197 @@
+import cv2
+
+class NaoOcorre:
+    def __init__(self):
+        self._00 = 1
+        self._01 = 1
+        self._10 = 1
+        self._11 = 1
+
+class Classe:
+    def __init__(self, caractere, n_dim):
+        self.caractere = caractere
+        self.n_dim = n_dim
+        self.n_restricoes = 0
+        self.NOC = []
+        for i in range(n_dim - 1):
+            self.NOC.append(NaoOcorre())
+        
+class ClassificacaoCaractere:
+    def __init__(self,altura,largura,tipo,flag):
+        self.altura = altura
+        self.largura = largura
+        self.tipo = tipo
+        self.n_classes = 0
+        self.n_dim = altura*largura
+        self.classes = []
+
+        if tipo == 1:
+            dig_numeros = "0123456789"
+            self.n_classes = 10
+            for i in range(self.n_classes):
+                self.classes.append(Classe(dig_numeros[i],self.n_dim))
+            if flag=='S':
+                self.monta_arq_aprendizado(dig_numeros,"numeros.png","numeros.txt")
+            self.incializa_classificador_2pixels("numeros.txt")
+        else:
+            dig_letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            self.n_classes = 26
+            for i in range(self.n_classes):
+                self.classes.append(Classe(dig_letras[i],self.n_dim))
+            if flag=='S':
+                self.monta_arq_aprendizado(dig_letras,"letras.png","letras.txt")
+            self.incializa_classificador_2pixels("letras.txt")
+
+    def monta_arq_aprendizado(self,digitos,nome_img,nome_arq):
+        img = cv2.imread(nome_img)
+        arq = open(nome_arq,"a")
+
+        img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(img_cinza, 127, 255, type=cv2.THRESH_BINARY)
+
+        contours, hiearchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        lista = []
+        for rec in contours:
+            x,y,w,h = cv2.boundingRect(rec)
+            if h>30 and h<40:
+                #cv2.rectangle(img_cinza, (x, y), (x+w-1, y+h-1), (100,100,100), 1)
+                lista.append([x, y, x+w-1, y+h-1])
+       
+        lista.sort()
+        for i in range(len(lista)):
+            xi = lista[i][0]
+            yi = lista[i][1]
+            xf = lista[i][2]
+            yf = lista[i][3]
+            img_dig = img_cinza[yi:yf,xi:xf]
+
+            #cv2.imshow(''+str(i),img_dig)
+            #cv2.waitKey(0)
+            transicao = self.retornaTransicaoHorizontal(img_dig)
+            arq.write(digitos[i]+'|'+transicao+"|\n")
+        arq.close()
+
+    def retornaTransicaoHorizontal(self,img):
+        dim = (self.largura, self.altura)
+        img_res = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        transicao = ""
+        flag = True
+        for i in range(self.altura):
+            if flag:
+                for j in range(self.largura):
+                    if img_res[i][j] == 0:
+                        transicao += '0'
+                    else:
+                        transicao += '1'
+            else:
+                for j in range(self.largura-1,-1,-1):
+                    if img_res[i][j] == 0:
+                        transicao += '0'
+                    else:
+                        transicao += '1'    
+            flag = not flag    
+        return transicao
+
+
+    def incializa_classificador_2pixels(self,nome_arq):
+        arq = open( nome_arq,'r')
+        todos_dados = arq.readlines()
+
+        pos = 0
+        for linha in todos_dados:
+            self.classes[pos].n_restricoes = (self.n_dim - 1)*4
+            lista = linha.split('|')
+            transicao = lista[1]
+            for j in range(self.n_dim -1):
+                if transicao[j]=='0' and transicao[j+1]=='0' and self.classes[pos].NOC[j]._00 == 1:
+                    self.classes[pos].NOC[j]._00 = 0 #0=ocorre, 1=nao ocorre
+                    self.classes[pos].n_restricoes-=1;
+                if transicao[j]=='0' and transicao[j+1]=='1' and self.classes[pos].NOC[j]._01 == 1:
+                    self.classes[pos].NOC[j]._01 = 0 #0=ocorre, 1=nao ocorre
+                    self.classes[pos].n_restricoes-=1;
+                if transicao[j]=='1' and transicao[j+1]=='0' and self.classes[pos].NOC[j]._10 == 1:
+                    self.classes[pos].NOC[j]._10 = 0 #0=ocorre, 1=nao ocorre
+                    self.classes[pos].n_restricoes-=1;
+                if transicao[j]=='1' and transicao[j+1]=='1' and self.classes[pos].NOC[j]._11 == 1:
+                    self.classes[pos].NOC[j]._11 = 0 #0=ocorre, 1=nao ocorre
+                    self.classes[pos].n_restricoes-=1;                    
+            pos+=1
+        arq.close()
+
+    def reconheceCaractereTransicao_2pixels(self, transicao):
+        cont_NOC = []
+        caractere = ' '
+
+        for i in range(self.n_classes):
+            cont_NOC.append(0)
+            for j in range(self.n_dim - 1):
+                #se tem alguma restricao
+                if self.classes[i].NOC[j]._00 == 1 or self.classes[i].NOC[j]._01 == 1 or self.classes[i].NOC[j]._10 == 1 or self.classes[i].NOC[j]._11 == 1:
+                    if transicao[j]=='0' and transicao[j+1]=='0' and self.classes[i].NOC[j]._00 == 1:
+                        cont_NOC[i]+=1
+                    if transicao[j]=='0' and transicao[j+1]=='1' and self.classes[i].NOC[j]._01 == 1:
+                        cont_NOC[i]+=1  
+                    if transicao[j]=='1' and transicao[j+1]=='0' and self.classes[i].NOC[j]._10 == 1:
+                        cont_NOC[i]+=1    
+                    if transicao[j]=='1' and transicao[j+1]=='1' and self.classes[i].NOC[j]._11 == 1:
+                        cont_NOC[i]+=1
+
+        menor = self.n_dim
+        pos = 0
+        for i in range(self.n_classes):
+            if cont_NOC[i] < menor:
+                menor = cont_NOC[i]
+                pos = i
+        #se empatar, verificar a classe com mais restricoes
+        maior = 0
+        for i in range(self.n_classes):
+            if menor == cont_NOC[i]:
+                if self.classes[i].n_restricoes > maior:
+                    maior = self.classes[i].n_restricoes
+                    pos = i
+        if menor < self.n_dim:
+            caractere = self.classes[pos].caractere
+        return caractere
+
+if __name__ == "__main__":
+    cl = ClassificacaoCaractere(30,40,2,'S')
+    img = cv2.imread("letra_l.png")
+    
+
+    img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img_cinza, 127, 255, type=cv2.THRESH_BINARY)
+
+    transicao = cl.retornaTransicaoHorizontal(thresh)
+    print("Previsto: ",cl.reconheceCaractereTransicao_2pixels(transicao))
+
+
+'''
+    #teste de reconhecimento de numeros
+    img = cv2.imread("numeros.png") #1
+    #img = cv2.imread("letras.png") #1
+
+    img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img_cinza, 127, 255, type=cv2.THRESH_BINARY)
+
+    contours, hiearchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    lista = []
+    for rec in contours:
+        x,y,w,h = cv2.boundingRect(rec)
+        if h>30 and h<40:
+            #cv2.rectangle(img_cinza, (x, y), (x+w-1, y+h-1), (100,100,100), 1)
+            lista.append([x, y, x+w-1, y+h-1])
+       
+    lista.sort()
+    for i in range(len(lista)):
+        xi = lista[i][0]
+        yi = lista[i][1]
+        xf = lista[i][2]
+        yf = lista[i][3]
+        img_dig = img_cinza[yi:yf,xi:xf]
+        cv2.imshow(''+str(i),img_dig)
+        transicao = cl.retornaTransicaoHorizontal(img_dig)
+        print(cl.reconheceCaractereTransicao_2pixels(transicao))
+        cv2.waitKey(0)        
+'''
+
+    
